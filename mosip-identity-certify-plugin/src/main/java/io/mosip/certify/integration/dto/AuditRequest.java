eb4def55fded9d19ec13d07c4dd479508f5dcf0f
Challarao <challarao@beehyv.com>
2024-06-18 12:02:44 +0530

    [INJICERT-189] change package name and exclude esignet core transitive dependency Signed-off-by: Challarao <challarao@beehyv.com>

---
+++ mosip-identity-certify-plugin/src/main/java/io/mosip/certify/integration/dto/AuditRequest.java
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.certify.integration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * The Class AuditRequestDto.
+ *
+ * @author Manoj SP
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuditRequest {
+	
+	private String eventId;
+	private String eventName;
+	private String eventType;
+	private LocalDateTime actionTimeStamp;
+	private String hostName;
+	private String hostIp;
+	private String applicationId;
+	private String applicationName;
+	private String sessionUserId;
+	private String sessionUserName;
+	private String id;
+	private String idType;
+	private String createdBy;
+	private String moduleName;
+	private String moduleId;
+	private String description;
+
+}
---
+++ mosip-identity-certify-plugin/src/test/java/io/mosip/certify/integration/service/HelperServiceTest.java
@@ -0,0 +1,36 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.certify.integration.service;
+
+
+import io.mosip.kernel.signature.dto.JWTSignatureResponseDto;
+import io.mosip.kernel.signature.service.SignatureService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class HelperServiceTest {
+
+    @InjectMocks
+    private HelperService helperService;
+
+    @Mock
+    private SignatureService signatureService;
+
+    @Test
+    public void getRequestSignature_validation() {
+        JWTSignatureResponseDto jwtSignatureResponseDto = new JWTSignatureResponseDto();
+        jwtSignatureResponseDto.setJwtSignedData("test-jwt");
+        Mockito.when(signatureService.jwtSign(Mockito.any())).thenReturn(jwtSignatureResponseDto);
+        Assert.assertEquals("test-jwt", helperService.getRequestSignature("test-request-value"));
+    }
+}
