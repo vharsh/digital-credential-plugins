61c462a08d47960ef0dd28574322acaa7d7f1f58
Challarao <challarao@beehyv.com>
2024-06-20 17:18:35 +0530

    [MOSIP-33939] move develop changes to release branch (#39)
    * [INJICERT-189] move mosip id changes from feature branch to develop (#32)
    
    * [INJICERT-189] add mosip id plugin implementation module
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * Update push-trigger.yml
    Signed-off-by: Praful Rakhade <99539100+Prafulrakhade@users.noreply.github.com>
    
    * [INJICERT-189] add esignet dependency for redis cache share
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * [INJICERT-189] change package name and exclude esignet core transitive dependency
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * [INJICERT-189] add test dependencies and fix workflow issue
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * [INJICERT-189] make service classes as conditional beans
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * [INJICERT-189] add esignet integration api dependency
    Signed-off-by: Challarao <challarao@beehyv.com>
    
    * [MOSIP-33939] change esignet dependency versions for mosip ida plugin
    
    Signed-off-by: Challa Rao <challarao@beehyv.com>
    
    ---------
    
    Signed-off-by: Praful Rakhade <99539100+Prafulrakhade@users.noreply.github.com>
    Signed-off-by: Challa Rao <challarao@beehyv.com>
    Co-authored-by: Vishwa <visu.vs1@gmail.com>
    Co-authored-by: Praful Rakhade <99539100+Prafulrakhade@users.noreply.github.com>
    Co-authored-by: Chandra Keshav Mishra <chandrakeshavmishra@gmail.com>
    Signed-off-by: Vishwa <visu.vs1@gmail.com>

---
+++ mosip-identity-certify-plugin/src/main/java/io/mosip/certify/mosipid/integration/service/HelperService.java
@@ -0,0 +1,63 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package io.mosip.certify.mosipid.integration.service;
+
+
+import io.mosip.kernel.signature.dto.JWTSignatureRequestDto;
+import io.mosip.kernel.signature.dto.JWTSignatureResponseDto;
+import io.mosip.kernel.signature.service.SignatureService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import java.nio.charset.StandardCharsets;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
+
+@Service
+@Slf4j
+@ConditionalOnProperty(value = "mosip.certify.integration.vci-plugin", havingValue = "IdaVCIssuancePluginImpl")
+public class HelperService {
+
+    public static final String UTC_DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    public static final String OIDC_PARTNER_APP_ID = "CERTIFY_PARTNER";
+    private static Base64.Encoder urlSafeEncoder;
+
+    static {
+        urlSafeEncoder = Base64.getUrlEncoder().withoutPadding();
+    }
+
+
+    @Autowired
+    private SignatureService signatureService;
+
+    protected String getRequestSignature(String request) {
+        JWTSignatureRequestDto jwtSignatureRequestDto = new JWTSignatureRequestDto();
+        jwtSignatureRequestDto.setApplicationId(OIDC_PARTNER_APP_ID);
+        jwtSignatureRequestDto.setReferenceId("");
+        jwtSignatureRequestDto.setIncludePayload(false);
+        jwtSignatureRequestDto.setIncludeCertificate(true);
+        jwtSignatureRequestDto.setDataToSign(HelperService.b64Encode(request));
+        JWTSignatureResponseDto responseDto = signatureService.jwtSign(jwtSignatureRequestDto);
+        log.debug("Request signature ---> {}", responseDto.getJwtSignedData());
+        return responseDto.getJwtSignedData();
+    }
+
+    protected static String getUTCDateTime() {
+        return ZonedDateTime
+                .now(ZoneOffset.UTC)
+                .format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN));
+    }
+
+    protected static String b64Encode(String value) {
+        return urlSafeEncoder.encodeToString(value.getBytes(StandardCharsets.UTF_8));
+    }
+
+}
