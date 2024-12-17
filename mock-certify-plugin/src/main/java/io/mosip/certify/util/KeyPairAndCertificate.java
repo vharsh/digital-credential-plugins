f352d7d607d70042964b5d3d6490508f8af8988c
Piyush7034 <piyushshukla2100@gmail.com>
2024-12-17 12:15:01 +0530

    Removed mdoc and mdl related code
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- mock-certify-plugin/src/main/java/io/mosip/certify/util/KeyPairAndCertificate.java
+++
@@ -1,7 +0,0 @@
-package io.mosip.certify.util;
-
-import java.security.KeyPair;
-import java.security.cert.X509Certificate;
-
-public record KeyPairAndCertificate(KeyPair keyPair, X509Certificate certificate) {
-}
